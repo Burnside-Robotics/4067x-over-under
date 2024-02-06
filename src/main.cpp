713fd568543dec354d629799f81b60783951dc6c
Alex Cutforth <alex@densem.net>
2024-02-06 19:59:52 +1300

    comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/**
+ * A dampener limits the rate at which a value can change. This is useful for adding sensitivity limits to joysticks.
+ */
 class Dampener {
 	private:
 		float currentValue = 0;
@@ -57,13 +60,16 @@ void initialize() {
 	pros::lcd::set_text(1, "Hello PROS User!");
 
 	sylib::initialize();
+	// Set the LEDs on the back of the robot to cycle through a rainbow gradient
 	backLights.gradient(0x990000, 0x990003, 45, 0, false, true);
 	backLights.cycle(*backLights, 5);
 
 
+	// Tell motors to apply brake force when commanded to apply zero voltage
 	leftDriveGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
 	rightDriveGroup.setBrakeMode(AbstractMotor::brakeMode::brake);
 
+	// Declare that the drive train uses Blue gearsets in the motors
 	leftDriveGroup.setGearing(AbstractMotor::gearset::blue);
 	leftDriveGroup.setGearing(AbstractMotor::gearset::blue);
 }
@@ -79,11 +85,10 @@ void autonomous() {
 }
 
 void opcontrol() {
-	ControllerButton catapultManualButton(ControllerDigital::right);
-
 	bool leftWingDeployed = false;
 	bool rightWingDeployed = false;
 
+	// Outer rear buttons on controller paddles
 	ControllerButton leftWingButton(ControllerDigital::down);
 	ControllerButton rightWingButton(ControllerDigital::B);
 
@@ -91,9 +96,11 @@ void opcontrol() {
 	Dampener leftInputDampener(0.4);
 	Dampener rightInputDampener(0.4);
 
+	// Inner right button on rear controller paddles
 	ControllerButton driveDirectionButton(ControllerDigital::Y);
 
 	bool driveReversed = false;
+	
 
 	bool elevationDeployed = false;
 	ControllerButton elevationToggle(ControllerDigital::R1);
@@ -106,10 +113,13 @@ void opcontrol() {
 		float catapultVoltage;
 
 		if(controller.getDigital(ControllerDigital::L1)) {
+			// Drive catapult at full speed
 			catapultVoltage = 12000;
 		} else if(controller.getDigital(ControllerDigital::L2)) {
+			// Drive catapult at reduced speed (suitable for match loading)
 			catapultVoltage = 9000;
 		} else if (controller.getDigital(ControllerDigital::right)) {
+			// Drive catapult backwards (should not be run  for more than a few seconds or it will strip)
 			catapultVoltage = -9000;
 		} else {
 			catapultVoltage = 0;
@@ -143,6 +153,7 @@ void opcontrol() {
 		/** ----------- **/
 
 		if (driveDirectionButton.changedToPressed()) {
+			// Switch the direction of the drive train and rumble the controller
 			driveReversed = !driveReversed;
 			controller.rumble(".");
 		}
@@ -151,7 +162,9 @@ void opcontrol() {
 		float rightInput = controller.getAnalog(ControllerAnalog::rightY);
 
 		if (driveReversed) {
+			// When the drive train is reversed, we swap the inputs, and reverse their polarity
 			std::swap(leftInput, rightInput);
+
 			leftInput *= -1;
 			rightInput *= -1;
 		}
@@ -160,6 +173,7 @@ void opcontrol() {
 
 		OdomState state = driveTrain->getState();
 
+		// Print the odometry position to the LCD screen
 		pros::lcd::print(2, "X: %f", state.x.convert(inch));
 		pros::lcd::print(3, "Y: %f", state.y.convert(inch));
 		pros::lcd::print(4, "theta: %f", state.theta.convert(degree));
